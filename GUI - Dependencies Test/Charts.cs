7f953f6a68e7caa790a3dcd5b916e1b3312120f9
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-07-30 16:04:57 -0600

    ...

---
+++ GUI - Dependencies Test/Charts.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GUI___Dependencies_Test
+{
+    public partial class Charts : UserControl
+    {
+        public Charts()
+        {
+            InitializeComponent();
+        }
+
+        private void Charts_Load(object sender, EventArgs e)
+        {
+            // Create a new chart
+            Chart pieChart = new Chart();
+
+            // Create a chart area
+            ChartArea chartArea = new ChartArea();
+            pieChart.ChartAreas.Add(chartArea);
+
+            // Create a series and add data points
+            Series series = new Series() { ChartType = SeriesChartType.Pie };
+            series.Points.AddXY("Category1", 10);
+            series.Points.AddXY("Category2", 20);
+            series.Points.AddXY("Category3", 30);
+            series.Points.AddXY("Category4", 40);
+
+            // Add the series to the chart
+            pieChart.Series.Add(series);
+
+            // Set the size and position of the chart
+            pieChart.Width = 300;
+            pieChart.Height = 300;
+            pieChart.Left = 10;
+            pieChart.Top = 10;
+
+            // Add the chart to the form
+            this.Controls.Add(pieChart);
+        }
+    }
+}
--- GUI - Dependencies Test/Form1.Designer.cs
+++ GUI - Dependencies Test/Form1.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             panel_LateralMenu = new Panel();
-            panel3 = new Panel();
-            button1 = new Button();
-            panel_Body = new Panel();
-            panel_UpMenu = new Panel();
-            button_Close = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
             button5 = new Button();
             button6 = new Button();
             button7 = new Button();
             button8 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            panel3 = new Panel();
+            button1 = new Button();
+            panel_Body = new Panel();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            panel_UpMenu = new Panel();
+            button_Close = new Button();
             panel_LateralMenu.SuspendLayout();
             panel_Body.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             panel_UpMenu.SuspendLayout();
             SuspendLayout();
             // 
@@ -64,98 +69,6 @@
             panel_LateralMenu.Size = new Size(246, 781);
             panel_LateralMenu.TabIndex = 0;
             panel_LateralMenu.MouseDown += panel_LateralMenu_MouseDown;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = SystemColors.MenuHighlight;
-            panel3.Location = new Point(3, 12);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(18, 42);
-            panel3.TabIndex = 1;
-            // 
-            // button1
-            // 
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(27, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(213, 42);
-            button1.TabIndex = 0;
-            button1.Text = "Video";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // panel_Body
-            // 
-            panel_Body.BackColor = SystemColors.Window;
-            panel_Body.Controls.Add(panel_UpMenu);
-            panel_Body.Dock = DockStyle.Fill;
-            panel_Body.Location = new Point(246, 0);
-            panel_Body.Name = "panel_Body";
-            panel_Body.Size = new Size(1107, 781);
-            panel_Body.TabIndex = 1;
-            panel_Body.MouseDown += panel_Body_MouseDown;
-            // 
-            // panel_UpMenu
-            // 
-            panel_UpMenu.BackColor = SystemColors.MenuBar;
-            panel_UpMenu.Controls.Add(button_Close);
-            panel_UpMenu.Dock = DockStyle.Top;
-            panel_UpMenu.Location = new Point(0, 0);
-            panel_UpMenu.Name = "panel_UpMenu";
-            panel_UpMenu.Size = new Size(1107, 54);
-            panel_UpMenu.TabIndex = 0;
-            panel_UpMenu.MouseDown += panel_UpMenu_MouseDown;
-            // 
-            // button_Close
-            // 
-            button_Close.Dock = DockStyle.Right;
-            button_Close.FlatAppearance.BorderSize = 0;
-            button_Close.FlatAppearance.MouseOverBackColor = Color.FromArgb(192, 0, 0);
-            button_Close.FlatStyle = FlatStyle.Flat;
-            button_Close.Location = new Point(1051, 0);
-            button_Close.Name = "button_Close";
-            button_Close.Size = new Size(56, 54);
-            button_Close.TabIndex = 0;
-            button_Close.Text = "X";
-            button_Close.UseVisualStyleBackColor = true;
-            button_Close.Click += button_Close_Click;
-            // 
-            // button2
-            // 
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(27, 60);
-            button2.Name = "button2";
-            button2.Size = new Size(213, 42);
-            button2.TabIndex = 2;
-            button2.Text = "Video";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Location = new Point(27, 156);
-            button3.Name = "button3";
-            button3.Size = new Size(213, 42);
-            button3.TabIndex = 4;
-            button3.Text = "Video";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.FlatAppearance.BorderSize = 0;
-            button4.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
-            button4.FlatStyle = FlatStyle.Flat;
-            button4.Location = new Point(27, 108);
-            button4.Name = "button4";
-            button4.Size = new Size(213, 42);
-            button4.TabIndex = 3;
-            button4.Text = "Video";
-            button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
@@ -205,6 +118,116 @@
             button8.Text = "Video";
             button8.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(27, 156);
+            button3.Name = "button3";
+            button3.Size = new Size(213, 42);
+            button3.TabIndex = 4;
+            button3.Text = "Video";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(27, 108);
+            button4.Name = "button4";
+            button4.Size = new Size(213, 42);
+            button4.TabIndex = 3;
+            button4.Text = "Video";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(27, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(213, 42);
+            button2.TabIndex = 2;
+            button2.Text = "Video";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.MenuHighlight;
+            panel3.Location = new Point(3, 12);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(18, 42);
+            panel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(27, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(213, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Video";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel_Body
+            // 
+            panel_Body.BackColor = SystemColors.Window;
+            panel_Body.Controls.Add(chart1);
+            panel_Body.Controls.Add(panel_UpMenu);
+            panel_Body.Dock = DockStyle.Fill;
+            panel_Body.Location = new Point(246, 0);
+            panel_Body.Name = "panel_Body";
+            panel_Body.Size = new Size(1107, 781);
+            panel_Body.TabIndex = 1;
+            panel_Body.MouseDown += panel_Body_MouseDown;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            chart1.Dock = DockStyle.Fill;
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(0, 54);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(1107, 727);
+            chart1.TabIndex = 1;
+            chart1.Text = "chart1";
+            // 
+            // panel_UpMenu
+            // 
+            panel_UpMenu.BackColor = SystemColors.MenuBar;
+            panel_UpMenu.Controls.Add(button_Close);
+            panel_UpMenu.Dock = DockStyle.Top;
+            panel_UpMenu.Location = new Point(0, 0);
+            panel_UpMenu.Name = "panel_UpMenu";
+            panel_UpMenu.Size = new Size(1107, 54);
+            panel_UpMenu.TabIndex = 0;
+            panel_UpMenu.MouseDown += panel_UpMenu_MouseDown;
+            // 
+            // button_Close
+            // 
+            button_Close.Dock = DockStyle.Right;
+            button_Close.FlatAppearance.BorderSize = 0;
+            button_Close.FlatAppearance.MouseOverBackColor = Color.FromArgb(192, 0, 0);
+            button_Close.FlatStyle = FlatStyle.Flat;
+            button_Close.Location = new Point(1051, 0);
+            button_Close.Name = "button_Close";
+            button_Close.Size = new Size(56, 54);
+            button_Close.TabIndex = 0;
+            button_Close.Text = "X";
+            button_Close.UseVisualStyleBackColor = true;
+            button_Close.Click += button_Close_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -218,6 +241,7 @@
             MouseDown += Form1_MouseDown;
             panel_LateralMenu.ResumeLayout(false);
             panel_Body.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
             panel_UpMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
@@ -237,5 +261,6 @@
         private Button button3;
         private Button button4;
         private Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
