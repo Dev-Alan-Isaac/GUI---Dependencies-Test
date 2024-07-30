2d4397ce51923cd2342f9e2af163f19083b847db
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-07-30 15:57:16 -0600

    Update Form1.Designer.cs

--- GUI - Dependencies Test/Form1.Designer.cs
+++ GUI - Dependencies Test/Form1.Designer.cs
@@ -34,6 +34,13 @@
             panel_Body = new Panel();
             panel_UpMenu = new Panel();
             button_Close = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
             panel_LateralMenu.SuspendLayout();
             panel_Body.SuspendLayout();
             panel_UpMenu.SuspendLayout();
@@ -42,6 +49,13 @@
             // panel_LateralMenu
             // 
             panel_LateralMenu.BackColor = SystemColors.Menu;
+            panel_LateralMenu.Controls.Add(button5);
+            panel_LateralMenu.Controls.Add(button6);
+            panel_LateralMenu.Controls.Add(button7);
+            panel_LateralMenu.Controls.Add(button8);
+            panel_LateralMenu.Controls.Add(button3);
+            panel_LateralMenu.Controls.Add(button4);
+            panel_LateralMenu.Controls.Add(button2);
             panel_LateralMenu.Controls.Add(panel3);
             panel_LateralMenu.Controls.Add(button1);
             panel_LateralMenu.Dock = DockStyle.Left;
@@ -107,6 +121,90 @@
             button_Close.UseVisualStyleBackColor = true;
             button_Close.Click += button_Close_Click;
             // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(27, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(213, 42);
+            button2.TabIndex = 2;
+            button2.Text = "Video";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(27, 156);
+            button3.Name = "button3";
+            button3.Size = new Size(213, 42);
+            button3.TabIndex = 4;
+            button3.Text = "Video";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(27, 108);
+            button4.Name = "button4";
+            button4.Size = new Size(213, 42);
+            button4.TabIndex = 3;
+            button4.Text = "Video";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(27, 348);
+            button5.Name = "button5";
+            button5.Size = new Size(213, 42);
+            button5.TabIndex = 8;
+            button5.Text = "Video";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(27, 300);
+            button6.Name = "button6";
+            button6.Size = new Size(213, 42);
+            button6.TabIndex = 7;
+            button6.Text = "Video";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(27, 252);
+            button7.Name = "button7";
+            button7.Size = new Size(213, 42);
+            button7.TabIndex = 6;
+            button7.Text = "Video";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatAppearance.MouseOverBackColor = SystemColors.HotTrack;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(27, 204);
+            button8.Name = "button8";
+            button8.Size = new Size(213, 42);
+            button8.TabIndex = 5;
+            button8.Text = "Video";
+            button8.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -132,5 +230,12 @@
         private Button button1;
         private Panel panel_UpMenu;
         private Button button_Close;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button3;
+        private Button button4;
+        private Button button2;
     }
 }
